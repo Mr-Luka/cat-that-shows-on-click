81004b0777f9169003ea43a972cfaf904355159c
Luka <luka.luka.luka93@gmail.com>
2023-03-22 15:24:03 -0700

    Changed the style of a button on click

--- js/script.js
+++ js/script.js
@@ -2,7 +2,15 @@ var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
 button.addEventListener("click", function () {
-    button.innerText = "It's Working!!!";
-    cat.classList.add("show");
+    if (cat.classList.contains("show")) {
+        //console.log("Yes!");
+        cat.classList.remove("show");
+        button.innerText = "Wait, come back!."
+        button.classList.add("disappear");
+    } else {
+        //console.log("No!");
+        cat.classList.add("show");
+        button.innerText = "Shoo, cat!!"
+        button.classList.remove("disappear");
+    }
 });
-
